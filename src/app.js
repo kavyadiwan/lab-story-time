e6d964bd62c42742b5a81bcbbebe674851678325
kavyadiwan <70085610+kavyadiwan@users.noreply.github.com>
2020-10-11 11:06:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,33 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
-
+function moreAboutHome(address, distanceFromTown, hasNeighbours){
+    var text = typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours);
+    return text;
+}
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily){
+    if(typeof(parents)=="string" && typeof(noOfSiblings)=="number" && typeof(isNuclearFamily)=="boolean"){
+return true
+}
+else{
+return false
+}
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber){
+    if(isNaN(ageInText)){
+        return ageInText
+    }else{
+      return ag
+    }
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
